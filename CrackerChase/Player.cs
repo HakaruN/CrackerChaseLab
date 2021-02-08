280af6fc74d73bf32e028f7749a867b0f0d3ac63
jturner1998 <mrjturner1998@gmail.com>
2021-02-08 11:03:14 +0000

    Created Player Class

---
+++ CrackerChase/Player.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackerChase
+{
+    class Player
+    {
+        Mover mainPlayer;
+    }
+}
