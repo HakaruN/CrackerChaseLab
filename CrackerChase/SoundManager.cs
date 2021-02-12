54a334e46439cace0ee7aabcb481187de35ac5e7
soapy98 <53182878+soapy98@users.noreply.github.com>
2021-02-12 10:39:40 +0000

    SoundManager

---
+++ CrackerChase/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackerChase
+{
+    class SoundManager
+    {
+        SoundEffect death;
+        SoundEffect gunShoot;
+        SoundEffect background;
+        SoundEffect enemyCol;
+
+        ContentManager content;
+        SoundManager(ContentManager cont)
+        {
+            content = cont;
+        }
+
+
+
+    }
+}
