36f4d691c1107f52d8b4c5794d63e4478a1b37eb
= <=>
2021-02-12 12:38:13 +0000

    added movement logic to the enemies, doesnt yet work

--- CrackerChase/gameplayScene.cs
+++ CrackerChase/gameplayScene.cs
@@ -17,26 +17,47 @@ namespace CrackerChase
             mEnemies = enemies;
             mBarricades = barricades;
             gameScore = 0;
+            mEnemiesXPos = enemiesXPos;
+            mEnemiesYPos = enemiesYPos;
         }
-        /*
-        public GameplayScene(Enemy enemy)
-        {
-            //mEnemy = enemy;
-            mEnemies = enemies;
-        }
-        */
+
 
         public void Update(GameTime gametime, KeyboardState keys, SceneManager manager, int inScreenWidth, int inScreenHeight)
         {
             //call update method on the player
             mPlayer.Update(1/60f, keys, inScreenWidth, inScreenHeight);
-            
+
             ///mEnemy.Update(gametime);
 
             //call update method on the aliens
-            
-            for(int i = 0; i < mEnemies.Count(); i++)
+
+            //set enemy to change direction when they reach 10 pixels from the screen side
+            if(mEnemiesXPos <= 10)
             {
+                enemyMoveDir = false;
+                mEnemiesYPos = mEnemiesYPos - 10;
+            }
+            else if(mEnemiesXPos >= inScreenWidth - 10)
+            {
+                enemyMoveDir = true;
+                mEnemiesYPos = mEnemiesYPos - 10;
+            }
+
+            //apply position changes
+            if(enemyMoveDir == false)
+            {
+                    mEnemiesXPos = mEnemiesXPos - 10;
+            }
+            else
+            {
+                mEnemiesXPos = mEnemiesXPos + 10;
+            }
+            
+            
+
+            for (int i = 0; i < mEnemies.Count(); i++)
+            {
+                mEnemies[i].SetPosition(mEnemies[i].GetPos().X + mEnemiesXPos, mEnemies[i].GetPos().Y + mEnemiesYPos);
                mEnemies[i].Update(gametime);
             }
 
@@ -68,6 +89,8 @@ namespace CrackerChase
         }
 
         int gameScore;
+        bool enemyMoveDir = false;
+        int mEnemiesXPos, mEnemiesYPos;
         Player mPlayer;//player
         List<Enemy> mEnemies;//list of the aliens
         List<Barricade> mBarricades;
